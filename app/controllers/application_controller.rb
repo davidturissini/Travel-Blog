80e04d41249022c1c30a236b4966427827c73937
Dave Turissini <davidairborne@gmail.com>
2012-07-14 02:16:14 -0700

    Adding navigation skeleton

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :navigation_content
+
+  def navigation_content
+   @nav = {
+    :climbs => Climb.all
+   }
+  end
+
   def render_404
    render :file => "public/404.html", :status => 404, :layout => false
   end
