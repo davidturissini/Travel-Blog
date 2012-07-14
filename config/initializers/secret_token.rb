f3ba09082d17459b8cfb2afb50f3f895e743fb8b
David Turissini <dave@bytemagik.com>
2012-07-14 00:09:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adventureblog::Application.config.secret_token = 'b35f3a726330dc6b44755b77326912bbaefe64241b01bd52adfca5d58c89ff946da97771ea468fc4b017f4621fc2384675bbff89a1a5cb672c33ebbd6e92fbed'
