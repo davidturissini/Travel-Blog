d79da999abe9cb97ca6c7a8f81da1baa528304cc
Dave Turissini <davidairborne@gmail.com>
2012-07-15 02:41:50 -0700

    Moving location links to helper method

---
+++ app/helpers/location_helper.rb
@@ -0,0 +1,10 @@
+module LocationHelper
+ def location_path loc
+  user_location_type_location_path(loc.user.id, loc.location_type.slug, loc.slug)
+ end
+
+ def location_link loc
+  path = location_path loc
+  link_to loc.title, path, { :class => "location #{loc.location_type.title.downcase}" }  
+ end
+end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
  belongs_to :location_type
+ has_one :user, :through => :location_type
 end
