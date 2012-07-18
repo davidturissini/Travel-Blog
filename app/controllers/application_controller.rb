82ad227ea60dc22d289863dce18fff8c158813dc
Dave Turissini <davidairborne@gmail.com>
2012-07-17 23:48:31 -0700

    current_user method for app_controller and login method for user.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+
+  def current_user
+   cookie = JSON.parse( cookies[:user] )
+   user = User.where({:id => cookie["id"], :token => cookie["token"]}).first
+   user || nil
+  end
 
   def render_404
    render :file => "public/404.html", :status => 404, :layout => false
   end
+
 end
