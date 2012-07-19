6e69f685c9d16c51b012a4751e96deea7e0836ab
Dave Turissini <davidairborne@gmail.com>
2012-07-18 21:47:04 -0700

    Schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716043816) do
+ActiveRecord::Schema.define(:version => 20120719035558) do
 
   create_table "journal_entries", :force => true do |t|
     t.string   "title"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(:version => 20120716043816) do
     t.datetime "updated_at"
     t.string   "salt"
     t.string   "token"
+    t.string   "slug"
   end
 
 end
