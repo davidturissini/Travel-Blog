b9ab9510bd943001488fcdbf8242aa1ef8e7f8dd
Dave Turissini <davidairborne@gmail.com>
2012-07-22 22:04:32 -0700

    Client side functionality

--- app/helpers/location_helper.rb
+++ app/helpers/location_helper.rb
@@ -3,8 +3,8 @@ module LocationHelper
   user_location_type_location_path(loc.user.slug, loc.location_type.slug, loc.slug)
  end
 
- def location_link loc
+ def location_link loc, options = {}
   path = location_path loc
-  link_to loc.title, path, { :class => "location #{loc.location_type.title.downcase}" }  
+  link_to loc.title, path, options.merge({ :class => "location #{loc.location_type.title.downcase}" })
  end
 end
