b92ecd65e83d4de47b4d9e19e4d17e3dfecebe5b
Dave Turissini <davidairborne@gmail.com>
2012-08-01 00:14:09 -0700

    Cleaning up location views

--- app/assets/javascripts/models.js
+++ app/assets/javascripts/models.js
@@ -85,11 +85,9 @@ var Location = Backbone.Model.extend({
  }
 })
 
-
-
-
-
-
-
-
-
+var FlickrImage = Backbone.Model.extend({
+ url: function (size) {
+  size = size ? "_" + size : ""
+  return "http://farm" + this.get("farm") + ".static.flickr.com/" + this.get("server") + "/" + this.get("id") + "_" + this.get("secret") + size + ".jpg"
+ } 
+})
