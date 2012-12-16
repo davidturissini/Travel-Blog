c2846469de32b7d9452ea1c3bd944e8027d85774
Dave Turissini <davidairborne@gmail.com>
2012-12-16 01:20:07 -0800

    Adding summary in right column

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,9 +2,7 @@ class MapsController < ApplicationController
 	def index
 		@user = User.find_by_slug(params[:user_id])
 		raise ActiveRecord::RecordNotFound if !@user
-		@trip = @user.trips.find_by_slug(params[:trip_id])
-		raise ActiveRecord::RecordNotFound if !@trip
-		@maps = @trip.maps
-		@locations = @trip.locations
+		@maps = @user.maps
+		@locations = @user.locations
 	end
 end
